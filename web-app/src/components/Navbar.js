e41f06a36d44ba0f36cac61890cef902103a9374
Ashu-Sangar <ashu.sangar18@gmail.com>
2024-05-20 15:10:47 -0400

    mhm

--- web-app/src/components/Navbar.js
+++ web-app/src/components/Navbar.js
@@ -2,19 +2,18 @@
 import Link from 'next/link';
 import styles from '@/css/navbar.css';
 
-
-
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/top-albums">Top Albums</Link></li>
-        <li><Link href="/top-artists">Top Artists</Link></li>
-        <li><Link href="/events">Upcoming Events</Link></li>
-        <li><Link href="/logout">Logout</Link></li>
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}><Link href="/">Home</Link></li>
+        <li className={styles.navItem}><Link href="/top-albums">Top Albums</Link></li>
+        <li className={styles.navItem}><Link href="/top-artists">Top Artists</Link></li>
+        <li className={styles.navItem}><Link href="/events">Upcoming Events</Link></li>
+        <li className={styles.navItem}><Link href="/logout">Logout</Link></li>
       </ul>
     </nav>
   );
 }
+
 export default Navbar;
